b4967e0e64e316dc37ed0268faa211a2be25773e
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-12-23 21:56:40 -0600

    feat: adjusting the desing

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -56,41 +56,65 @@ export const BentoGridItem = ({
         <div className="w-full h-full absolute">
           {img && (<img src={img} alt={img} className={cn(imgClassName, "object-cover object-center ")}></img>)}
         </div>
-        <div   className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
-            } `}>
-          {spareImg && (<img src={spareImg} alt={spareImg} className="object-cover object-center w-full h-full" ></img>)}
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
+          } `}>
+          {spareImg && (<img src={spareImg} alt={spareImg} className="object-cover object-center w-full h-full"></img>)}
         </div>
-        {id === 6 &&(
+        {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
           </BackgroundGradientAnimation>
         )}
 
-        <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10')}>
+        <div  className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}>
           <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
             {description}
           </div>
-          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
-          {title}
-        </div>
-        
-        {id === 2 && <GlobeDemo />}
-        {id === 3 && 
-          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-            <div className="flex flex-col gap-3 lg:gap-8">
-              {["React.js", "Next.js", "Spring Boot"].map((item) => (<span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>) )}
-              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
-            </div>
+          <div     className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}>
+            {title}
+          </div>
+
+          {id === 2 && <GlobeDemo />}
+          {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-            <div className="flex flex-col gap-3 lg:gap-8">
-              {["React.js", "Next.js", "Spring Boot"].map((item) => (<span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>) )}
-              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                {["React.js", "Next.js", "SpringBoot"].map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                {["MySQL", "Laravel", "Python"].map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+
+                
+              </div>
             </div>
-          </div>
-          </div>
-        }
+          )}
+             {id === 6 && (
+                  <div className="mt-5 relative">
+                    <div className={`absolute -bottom-5 right-0`}></div>
+                  </div>
+                )}
+        </div>
       </div>
-    </div>
     </div>
   );
 };

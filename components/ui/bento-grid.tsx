8a9be12ef14e6c1a7e138893dab6096095e70b87
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-12-23 21:25:12 -0600

    feat: including new sections to the bento-grid.tsx

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -43,7 +43,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
       style={{
@@ -52,12 +52,13 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
-          {img && (<img src={img} alt={img} className={cn(imgClassName), 'object-cover', 'object-center'}></img>)}
+          {img && (<img src={img} alt={img} className={cn(imgClassName, "object-cover object-center ")}></img>)}
         </div>
-        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
-          {spareImg && (<img src={spareImg} alt={spareImg} className={'object-cover', 'object-center w-full h-full'}></img>)}
+        <div   className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
+            } `}>
+          {spareImg && (<img src={spareImg} alt={spareImg} className="object-cover object-center w-full h-full" ></img>)}
         </div>
         {id === 6 &&(
           <BackgroundGradientAnimation>
@@ -65,16 +66,31 @@ export const BentoGridItem = ({
           </BackgroundGradientAnimation>
         )}
 
-        <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10 ')}>
+        <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10')}>
           <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
             {description}
           </div>
           <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
           {title}
         </div>
-        </div>
+        
         {id === 2 && <GlobeDemo />}
+        {id === 3 && 
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {["React.js", "Next.js", "Spring Boot"].map((item) => (<span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>) )}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+            </div>
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {["React.js", "Next.js", "Spring Boot"].map((item) => (<span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>) )}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+            </div>
+          </div>
+          </div>
+        }
       </div>
+    </div>
     </div>
   );
 };
